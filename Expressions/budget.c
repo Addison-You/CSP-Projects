27901fd20a9dfc6e58dba1a80924ece432467a84
Addison-You <Addison.jones@ucas-edu.net>
2024-09-12 20:29:03 +0000

    saving caculatar

---
+++ Expressions/budget.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    float month, income, rent, utilties, groceries, transportation 
+    printf("This going to caculate your budget for this month\n");
+    printf("How much do you make a month?\n");
+    scanf("%f", &month)
+    printf("Your income is$%.2f\n", income);
+    scanf("%f", &income)
+    printf("How much do is rent?\n")
+    scanf("%f", &rent)
+      printf("How much are groceries?\n");
+      scanf("%f", &groceries)
+      printf("How much do you spend on transportation?\n");
+      scanf("%f", &transportation)
+
+
+    return 0;
+}
