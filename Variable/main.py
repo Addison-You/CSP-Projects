e74b8738b446b86ad1bee67a6ccc6df8526178d2
Addison-You <Addison.jones@ucas-edu.net>
2024-09-18 19:41:10 +0000

    Hello world

--- Variable/main.py
+++ Variable/main.py
@@ -1,9 +1,10 @@
 
 name = "Jeff"
-print("Hello "+name+"!")
-name = "Bob"
-print("Hello "+name+"!")
-name = "Finn"
-print("Hello "+name+"!")
-name = "Addison"
-print("Hello "+name+"!")
+input("Hello "+name+"!")
+namee = "Bob"
+input("Hello "+name+"!")
+nameee = "Finn"
+input("Hello "+name+"!")
+nameeee = "Addison"
+input("Hello "+name+"!")
+print(name,) 
