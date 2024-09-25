1aeda8bc4f8271964bd5ec59d3420055845849a3
Addison-You <Addison.jones@ucas-edu.net>
2024-09-25 20:30:48 +0000

    Fix budget C

---
+++ Functions/Update_Budget.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+float income, rent, utilities, groceries, transportation, expenses, savings, total;
+
+float input(char type[], float var){
+      printf("How much does your %s: \n", type);
+      scanf("%f",&var);
+      return var;
+}
+
+
+void percent(char type[], int amount){
+    int per = amount/income*100;
+    printf("Your %s is %d%% of your income.\n", type, per);   
+}
+   
+
+    int main(void) { 
+    printf("This is going to calculate your budget for the month.");
+    printf("How much do you make in a month?\n");
+    income = input("income", income);
+    rent = input("rent", rent);
+    utilities = input("utilities", utilities);
+    groceries = input("groceries", groceries);
+    transportation = input("transportation", transportation);
+    expenses = rent + utilities + groceries + transportation;
+    savings = income *.2;
+    total = income - expenses - savings;
+    percent("rent", rent);
+    percent("utilittes", utilities);
+    percent("groceries", groceries);
+    percent("transportation", transportation);
+    percent("savings", savings);
+    percent("expenses", expenses);
+    return 0;
+}
+
