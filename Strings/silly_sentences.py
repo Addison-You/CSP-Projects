66af74c8c64c3c6b7fc68dfe4315b45fe466b8a4
Addison-You <Addison.jones@ucas-edu.net>
2024-09-25 19:43:02 +0000

    Silly safjhsafkj

---
+++ Strings/silly_sentences.py
@@ -0,0 +1,8 @@
+name = input("Type your name: ")
+place = input("Type a place: ")
+verb = input("Type a verb: ")
+noun = input("Type a noun: ")
+sentenenceOne = input(" went to the ")
+sentenenceTwo = input(" where they")
+sentenenceThree = input(" and bought a")
+print(name, sentenenceOne, place, sentenenceTwo, verb, sentenenceThree, noun)
