c08fdfbb5341277f448b33cc7d238282bb892cef
Addison-You <Addison.jones@ucas-edu.net>
2024-09-26 03:34:49 +0000

    update budget cac

--- Functions/Update_Budget.py
+++ Functions/Update_Budget.py
@@ -9,12 +9,19 @@ savings = income * 0.2
 total = income - expenses - savings
 prent = rent/income * 100
 putilities = utilities/income *100
-ptransportation = transportation/income * 100
+ptransportation = transportation/income * 100   
 pexpenses = expenses/income * 100
-print("Your income is: %\n", income)
-print("Your expenses are: %\n", expenses)
-print("Your savings are: %\n", savings)
-print("Your total left to spend is: $%.2f\n", total)
-print("Your rent is % of your income", prent)
-print("Your rent is % of your income", putilities)
-print("Your rent is % of your income", ptransportation)
+def percent(type, amount): 
+    per = amount/income *100
+
+    return f"Your {type} is {per}% income."
+
+print(f"Your monthly income is ${income:.2f}\n")
+print(f"Your monthly expenses are ${expenses:.2f}\n")
+print(f"Your monthly savings is ${savings:.2f}\n")
+print(percent("rent", rent))
+print(percent("utilities", utilities))
+print(percent("groceries", groceries))
+print(percent("transportation", transportation))
+print(percent("savings", savings))
+print(percent("expenses", expenses))
