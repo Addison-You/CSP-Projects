725bf9ad7cfdf11331003ca072ab78b9800160e5
Addison-You <Addison.jones@ucas-edu.net>
2024-10-01 20:06:48 +0000

    jkdg

--- Conditional/agecalc.py
+++ Conditional/agecalc.py
@@ -1,7 +1,18 @@
-age = "19"
-
+age = 19
 if age >= 18:
     print("you are old enough to vote!")
+
+if age >= 16:
+    print("you are old enough to drive!")
+
+if age >= 15:
+    print("you are old enough to get your learners permit!")
+
+if age >= 5:
+    print("you are old enough to go to school!")
+
+else: 
+    print("Couple more years for you!")
 
 
 
