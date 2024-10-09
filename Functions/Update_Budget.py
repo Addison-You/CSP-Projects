a05f98f0671655634b4ed9a06588da66c57beb76
Addison-You <Addison.jones@ucas-edu.net>
2024-10-09 18:34:52 +0000

    Update calc in Py

--- Functions/Update_Budget.py
+++ Functions/Update_Budget.py
@@ -5,21 +5,24 @@ rent = float (input("What is your monthly rent: "))
 utilities = float (input("What is your monthly utilities: "))
 groceries = float (input("What is your monthly groceries: "))
 transportation = float (input("What is your monthly transportation: "))
-expenses = rent + utilities + groceries + transportation
 savings = income *.2
+expenses = rent + utilities + groceries + transportation
 total = income - expenses - savings
 prent = rent/income * 100
 putilities = utilities/income *100
-ptransportation = transportation/income * 100   
+pgroceries = groceries/income *100
+ptransportation = transportation/income * 100
+psavings = savings/income *100 
 pexpenses = expenses/income * 100
-def percent(type, amount): 
+def percent(type, amount):
     per = amount/income *100
 
-    return f"Your {type} is {per}% income."
+    return (f"Your {type} is {per}% income. ")
 
-print(f"Your monthly income is ${income:.2f}\n")
-print(f"Your monthly expenses are ${expenses:.2f}\n")
-print(f"Your monthly savings is ${savings:.2f}\n")
+
+print(f"Your monthly income is ${income}\n")
+print(f"Your monthly expenses are ${expenses}\n")
+print(f"Your monthly savings is ${savings}\n")
 print(percent("rent", rent))
 print(percent("utilities", utilities))
 print(percent("groceries", groceries))
