eb4a8e08be89d2c9bbb160386513ec2829d27a6d
Addison-You <Addison.jones@ucas-edu.net>
2024-10-09 00:21:55 +0000

    Family loop in c

---
+++ loops/FamilyLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char siblings[5][20] = {"Finn", "Addison", "Julian", "Mariela", "Monica"};
+
+int i;
+int main(){
+     while (i < 5)
+          printf("%s Jones\n", siblings[i]);
+          i+=1;
+     }
+    return 0;
+} 
