c91f894d4b19a1568727d89d68b0ce3ffed5cb8a
Addison-You <Addison.jones@ucas-edu.net>
2024-10-10 20:42:08 +0000

    hii

--- loops/FizzBuzz.c
+++ loops/FizzBuzz.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int i, user;
+char one[50], two[50], three[50];
+
+
+void delay(int sec){
+    int milli = 1000*sec;
+
+    clock_t start = clock();
+
+    while(clock()< start+milli);
+}
 
 int main(){
-    //create a loop that counts to 50
+        printf("What number do you want it to go to? \n");
+        scanf("%d", &user);
+        printf("Give me a word: \n");
+        scanf("%s", one);
+        printf("Give me a word: \n");
+        scanf("%s", two);
+        strcat(three, one);
+        strcat(three, two);
 
-    //replace #'s divisable by 3 with "Fizz"
+    //create a loop that counts to 50
+    for(i=1;i<=user;i++){
+      
+    //replace #'s divisable by both with "FizzBuzz" 
+    if (i%3==0 && i%5==0){
+        printf("%s\n", three);
+
+
+    //replace #'s divisable by 3 with "Fizz" 
+    }else if (i%3==0){
+              printf("%s\n", one);
 
     //replace #'s divisable by 5 with "Buzz"
+    }else if (i%5==0){
+              printf("%s\n", two);
+    
+    //Print all other numbers  
+    }else{ 
+    printf("%d\n", i);
 
-    //replace #'s divisable by both with "FizzBuzz"
-
-    //Print all other numbers
-    printf("Hello World");
+    }  
+    delay(100);
+ }       
     return 0;
 }
