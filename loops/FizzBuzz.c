8ecd55911d4228ac2c239c646db2672bac606d9a
Addison-You <Addison.jones@ucas-edu.net>
2024-10-10 20:02:48 +0000

    d

---
+++ loops/FizzBuzz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    //create a loop that counts to 50
+
+    //replace #'s divisable by 3 with "Fizz"
+
+    //replace #'s divisable by 5 with "Buzz"
+
+    //replace #'s divisable by both with "FizzBuzz"
+
+    //Print all other numbers
+    printf("Hello World");
+    return 0;
+}
